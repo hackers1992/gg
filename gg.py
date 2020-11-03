c9cb89ef092477cb8c31cfb519fb6baa75b3ea6d
Raja <67794098+hackers1992@users.noreply.github.com>
2020-11-03 16:08:05 +0500

    Create gg.py

--- gg.py
+++ gg.py
@@ -115,7 +115,7 @@ def login():
 		    st = open("login.txt", "w")
 		    st.write(z["access_token"])
 		    st.close()
-		    print "\n[✓] Logged In Successfully."
+		print "\n[✓] Logged In Successfully."
 		    time.sleep(1)
 		    menu()
 		else:
