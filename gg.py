af10eb418374fdb09da7e6db71c3785f6af95d03
Raja <67794098+hackers1992@users.noreply.github.com>
2020-11-03 16:51:32 +0500

    Create gg.py

--- gg.py
+++ gg.py
@@ -56,114 +56,103 @@ vulnot = '\x1b[31mNot Vuln'
 vuln = '\x1b[32mVuln'
 
 
-def methodlogin():
+def login():
     os.system('clear')
     try:
-        toket = open('login.txt','r')
+        toket = open('login.txt', 'r')
         menu()
-    except (KeyError,IOError):
+    except (KeyError, IOError):
         os.system('clear')
         print logo
-        print "[1] Login With ID/Password"
-        print "[2] Login With Token"
-        print "[3] Back"
-        print
-        method_menu()
-def method_menu():
-	hos = raw_input("\nChoose Option >>  ")
-	if hos =="":
-		print"[!]  Wrong Input"
-		keluar()
-	elif hos =="1":
-		login()
-	elif hos =="2":
-		os.system('clear')
-		print logo
-		hosp = raw_input("[+] Give Token : ")
-		tik()
-		hopa = open('login.txt','w')
-		hopa.write(hosp)
-		hopa.close()
-		
-		time.sleep(1)
-		menu()
-		
-	elif hos =="0":
-		keluar()
-	else:
-		print"[!] Wrong Input"
-		keluar()
-def login():
-	os.system("clear")
-	try:
-		tb=open('login.txt', 'r')
-		menu()
-	except (KeyError,IOError):
-		os.system("clear")
-		print logo
-		jalan('[+] Login Your Facebook Account')
-		jalan('[!] Donot Use Your Personal Account')
-		jalan('[!] Use a New Facebook Account To Login')
-		print'-------------------------------------'
-		iid=raw_input('[+] Number/Email: ')
-		id=iid.replace(" ","")
-		pwd=raw_input('[+] Password : ')
-		tik()
-		data = br.open("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email="+(id)+"&locale=en_US&password="+(pwd)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		z=json.load(data)
-		if 'access_token' in z:
-		    st = open("login.txt", "w")
-		    st.write(z["access_token"])
-		    st.close()
-		
-		    time.sleep(1)
-		    menu()
-		else:
-		    if "www.facebook.com" in z["error_msg"]:
-		        print ('[!] User Must Verify Account Before Login.')
-		        time.sleep(3)
-		        login()
-		    else:
-		        print ('[!]Number/User Id/ Password Is Wrong !')
-		        time.sleep(1)
-		        login()
+        print 40 * '\x1b[1;97m\xe2\x95\x90'
+        print '\x1b[1;91m[\xe2\x98\x86] \x1b[1;92mLOGIN AKUN FACEBOOK AKUN FB \x1b[1;91m[\xe2\x98\x86]'
+        id = raw_input('\x1b[1;91m[+] \x1b[1;36mUsername FB \x1b[1;91m:\x1b[1;92m ')
+        pwd = getpass.getpass('\x1b[1;91m[+] \x1b[1;36mPassword FB \x1b[1;91m:\x1b[1;92m ')
+        tik()
+        try:
+            br.open('https://m.facebook.com')
+        except mechanize.URLError:
+            print '\n\x1b[1;91m[!] Tidak ada koneksi'
+            keluar()
+
+        br._factory.is_html = True
+        br.select_form(nr=0)
+        br.form['email'] = id
+        br.form['pass'] = pwd
+        br.submit()
+        url = br.geturl()
+        if 'save-device' in url:
+            try:
+                sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail=' + id + 'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword=' + pwd + 'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+                data = {'api_key': '882a8490361da98702bf97a021ddc14d', 'credentials_type': 'password', 'email': id, 'format': 'JSON', 'generate_machine_id': '1', 'generate_session_cookies': '1', 'locale': 'en_US', 'method': 'auth.login', 'password': pwd, 'return_ssl_resources': '0', 'v': '1.0'}
+                x = hashlib.new('md5')
+                x.update(sig)
+                a = x.hexdigest()
+                data.update({'sig': a})
+                url = 'https://api.facebook.com/restserver.php'
+                r = requests.get(url, params=data)
+                z = json.loads(r.text)
+                zedd = open('login.txt', 'w')
+                zedd.write(z['access_token'])
+                zedd.close()
+                print '\n\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mLogin berhasil'
+                requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token=' + z['access_token'])
+                os.system('xdg-open https://youtube.com/NjankSoekamti')
+                time.sleep(2)
+            except requests.exceptions.ConnectionError:
+                print '\n\x1b[1;91m[!] Tidak ada koneksi'
+                keluar()
+
+        if 'checkpoint' in url:
+            print '\n\x1b[1;91m[!] \x1b[1;93mAkun kena Checkpoint'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            keluar()
+        else:
+            print '\n\x1b[1;91m[!] Login Gagal'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            login()
+
+
 def menu():
-	os.system('clear')
-	try:
-		toket=open('login.txt','r').read()
-	except IOError:
-		os.system('clear')
-		print"\x1b[1;91m[!] Token invalid"
-		os.system('rm -rf login.txt')
-		time.sleep(1)
-		login()
-	try:
-		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
-		a = json.loads(otw.text)
-		nama = a['name']
-		id = a['id']
-		ots = requests.get('https://graph.facebook.com/me/subscribers?access_token=' + toket)
-		b = json.loads(ots.text)
-		sub = str(b['summary']['total_count'])
-	except KeyError:
-		os.system('clear')
-		print"\033[1;91mYour Account is on Checkpoint"
-		os.system('rm -rf login.txt')
-		time.sleep(1)
-		login()
-	except requests.exceptions.ConnectionError:
-		print"\x1b[1;92mThere is no internet connection"
-		keluar()
-	os.system("clear")
-	print logo
-	print "   \033[1;36m[*] Name\033[1;32;40m: "+nama+" "                               
-	print "   \033[1;36m[*] ID\033[1;36m: "+id+" "
-	print "   \033[1;36m[*] Subs\033[1;36m: "+sub+"  "
-	
-	print "   \033[1;36m[1] ══Start Hacking"	
-																														
-	print "   \033[1;36m[0] ══Log out"
-	pilih()
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r').read()
+    except IOError:
+        os.system('clear')
+        print '\x1b[1;91m[!] Token tidak ditemukan'
+        os.system('rm -rf login.txt')
+        time.sleep(1)
+        login()
+    else:
+        try:
+            otw = requests.get('https://graph.facebook.com/me?access_token=' + toket)
+            a = json.loads(otw.text)
+            nama = a['name']
+            id = a['id']
+        except KeyError:
+            os.system('clear')
+            print '\x1b[1;91m[!] \x1b[1;93mSepertinya akun kena Checkpoint'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            login()
+        except requests.exceptions.ConnectionError:
+            print '\x1b[1;91m[!] Tidak ada koneksi'
+            keluar()
+
+    os.system('clear')
+    print logo
+    print '\x1b[1;97m\xe2\x95\x94' + 40 * '\xe2\x95\x90'
+    print '\xe2\x95\x91\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m]\x1b[1;97m Nama \x1b[1;91m: \x1b[1;92m' + nama
+    print '\x1b[1;97m\xe2\x95\x9a' + 40 * '\xe2\x95\x90'
+    print '\x1b[1;37;40m1. Informasi Pengguna'
+    print '\x1b[1;37;40m2. Hack Akun Facebook'
+    print '\x1b[1;37;40m3. Bot               '
+    print '\x1b[1;37;40m4. Lainnya....       '
+    print '\x1b[1;37;40m5. LogOut            '
+    print '\x1b[1;31;40m0. Keluar            '
+    print
 
 
 def pilih():
@@ -176,7 +165,6 @@ def pilih():
             informasi()
         else:
             if zedd == '2':
-                menu_hack()
             else:
                 if zedd == '3':
                     menu_bot()
@@ -288,7 +276,6 @@ def menu_hack():
     print '\x1b[1;37;40m6. Ambil id/email/hp'
     print '\x1b[1;31;40m0. Kembali'
     print
-    hack_pilih()
 
 
 def hack_pilih():
@@ -305,7 +292,6 @@ def hack_pilih():
                 hasil()
             else:
                 if hack == '3':
-                    super()
                 else:
                     if hack == '4':
                         brute()
@@ -549,7 +535,6 @@ def super():
     print '\x1b[1;37;40m2. Crack dari member Grup'
     print '\x1b[1;31;40m0. Kembali'
     print
-    pilih_super()
 
 
 def pilih_super():
@@ -585,7 +570,6 @@ def pilih_super():
 
                 re = requests.get('https://graph.facebook.com/' + idg + '/members?fields=name,id&limit=999999999&access_token=' + toket)
                 s = json.loads(re.text)
-                for i in s['data']:
                     id.append(i['id'])
 
             else:
